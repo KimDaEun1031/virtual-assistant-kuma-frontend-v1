c66cdd69f31493bd360ec232ceda3d8871e8fe20
KimDaEun1031 <kimde1031@gmail.com>
2022-07-04 12:27:05 +0900

    refactor: Change singlequote to doubleqoute add: Add GlobalStyle

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import App from './app/App';
+import App from "./app/App";
+import GlobalStyle from "./component/shared/GlobalStyles";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Router>
+    <GlobalStyle />
     <App />
-  </React.StrictMode>
+  </Router>
 );
