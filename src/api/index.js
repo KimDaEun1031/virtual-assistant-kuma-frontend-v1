34f01fc96c567aab07d3f45b2e2461858720f740
KimDaEun1031 <kimde1031@gmail.com>
2022-07-19 19:57:14 +0900

    feat: Update api logic chore: 자잘한 수정

--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,9 @@ const API = Axios.create({ baseURL: process.env.REACT_APP_SERVER_URL });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("token")) {
-    req.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
+    req.headers.Token = localStorage.getItem("token");
+  } else if (localStorage.getItem("code")) {
+    req.headers.Code = localStorage.getItem("code");
   }
 
   return req;
@@ -35,6 +37,26 @@ export const getCalendarEvent = () => {
   return API.get("/calendar/auth");
 };
 
+export const getToken = (code) => {
+  return API.get("/calendar/token", {}, { headers: { code } });
+};
+
+export const getTodayEvent = () => {
+  return API.get("/calendar/event");
+};
+
+export const createEvent = (summary, date) => {
+  return API.post("/calendar/event", { summary, date });
+};
+
 export const getChatAnswer = (text) => {
   return API.post("/chat/textQuery", { text });
+};
+
+export const updateUser = (email, name, character) => {
+  return API.patch("/user", { email, name, character });
+};
+
+export const getWeatherInfo = () => {
+  return API.get("/weather");
 };
